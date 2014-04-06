ba89c79bee4f9cea8b8164c4b8247403060e837f
xtozero <xtozero@naver.com>
2014-04-07 01:26:40 +0900

    [xtozero] 스레드 풀 구현 추가.
    - 작업 등록 함수 추가
    - 스레드 풀 동작 함수 추가

--- xtozero/XtzThreadPool.cpp
+++ xtozero/XtzThreadPool.cpp
@@ -6,8 +6,8 @@ namespace xtozero
 	HANDLE CXtzThreadPool::m_thread[MAX_THREAD];
 	HANDLE CXtzThreadPool::m_threadEvent[MAX_THREAD];
 
-	LPVOID CXtzThreadPool::m_threadArg[MAX_THREAD];
-	WorkerFuntion CXtzThreadPool::m_worker[MAX_THREAD];
+	WORK CXtzThreadPool::m_work[MAX_THREAD];
+	bool CXtzThreadPool::m_bWork[MAX_THREAD];
 
 	CXtzThreadPool::CXtzThreadPool( ) : m_nThread( 0 )
 	{
@@ -19,6 +19,7 @@ namespace xtozero
 
 	void CXtzThreadPool::CreateThreadPool( int maxThread )
 	{
+		InitializeCriticalSection( &cs );
 		m_nThread = maxThread;
 
 		for ( int i = 0; i < MAX_THREAD; ++i )
@@ -30,16 +31,26 @@ namespace xtozero
 				(LPVOID)i,
 				0,
 				nullptr );
+			m_bWork[i] = false;
+			m_work[i].m_worker = nullptr;
 		}
 	}
 
 	DWORD WINAPI CXtzThreadPool::ThreadFunction( LPVOID arg )
 	{
+		int threadIdx = (DWORD)arg;
 		while ( true )
 		{
-			WaitForSingleObject( m_threadEvent[(DWORD)arg], INFINITE );
+			WaitForSingleObject( m_threadEvent[threadIdx], INFINITE );
 
-			m_worker[(DWORD)arg]( m_threadArg[(DWORD)arg] );
+			assert( m_work[threadIdx].m_worker );
+			if ( m_work[threadIdx].m_worker )
+			{
+				LPVOID& arg = m_work[threadIdx].m_arg;
+				m_work[threadIdx].m_worker( arg );
+			}
+
+			m_bWork[threadIdx] = false;
 		}
 	}
 
@@ -49,6 +60,51 @@ namespace xtozero
 		{
 			CloseHandle( m_threadEvent[i] );
 			CloseHandle( m_thread[i] );
+		}
+		DeleteCriticalSection( &cs );
+	}
+
+	void CXtzThreadPool::AddWork( WorkerFuntion worker, LPVOID arg )
+	{
+		EnterCriticalSection( &cs );
+
+		m_workquere.emplace_back( worker, arg );
+
+		LeaveCriticalSection( &cs );
+	}
+
+	void CXtzThreadPool::Run( void )
+	{
+		while ( true )
+		{
+			EnterCriticalSection( &cs );
+
+			if ( m_workquere.size() > 0 )
+			{
+				for ( int i = 0; i < m_nThread; ++i )
+				{
+					if ( m_bWork[i] )
+					{
+
+					}
+					else
+					{
+						WORK& work = m_workquere.front();
+						m_work[i].m_worker = work.m_worker;
+						m_work[i].m_arg = work.m_arg;
+						m_bWork[i] = true;
+						m_workquere.pop_front();
+						SetEvent( m_threadEvent[i] );
+						break;
+					}
+				}
+			}
+			else
+			{
+				break;
+			}
+
+			LeaveCriticalSection( &cs );
 		}
 	}
 }	
--- xtozero/XtzThreadPool.h
+++ xtozero/XtzThreadPool.h
@@ -1,11 +1,26 @@
 #ifndef _XTZTHREADPOOL_H_
 #define _XTZTHREADPOOL_H_
 
+#include <list>
+
 namespace xtozero
 {
 	const int MAX_THREAD = 8;
 
 	typedef void( *WorkerFuntion )(LPVOID);
+
+	CRITICAL_SECTION cs;
+
+	struct WORK
+	{
+		WORK() {}
+		WORK( WorkerFuntion worker, LPVOID arg )
+		: m_worker( worker ), m_arg( arg )
+		{}
+
+		WorkerFuntion	m_worker;
+		LPVOID			m_arg;
+	};
 
 	class CXtzThreadPool
 	{
@@ -16,9 +31,11 @@ namespace xtozero
 
 		static HANDLE m_thread[MAX_THREAD];
 		static HANDLE m_threadEvent[MAX_THREAD];
+		static bool m_bWork[MAX_THREAD];
 
-		static LPVOID m_threadArg[MAX_THREAD];
-		static WorkerFuntion m_worker[MAX_THREAD];
+		static WORK m_work[MAX_THREAD];
+
+		std::list<WORK> m_workquere;
 
 	public:
 		~CXtzThreadPool();
@@ -26,6 +43,8 @@ namespace xtozero
 		void CreateThreadPool(int maxThread);
 		void DestroyThreadPool();
 		static DWORD WINAPI ThreadFunction(LPVOID arg);
+		void AddWork( WorkerFuntion worker, LPVOID arg );
+		void Run( void );
 	};
 }
 
